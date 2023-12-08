6dd1cc09a0fa886c5414efd3965efe8dde289244
Jeremy E Kozdon <jeremy@kozdon.net>
2023-12-07 23:28:37 -0800

    day04
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- day06.cpp
+++ day06.cpp
@@ -1,6 +1,7 @@
 #include <filesystem>
 #include <fmt/core.h>
 #include <fstream>
+#include <ranges>
 #include <regex>
 #include <string>
 
@@ -13,25 +14,47 @@ void day06(const char *base) {
     exit(1);
   }
 
-  std::string line;
   std::regex num_regex("[0-9]+");
-  std::getline(input, line);
-  auto num_begin = std::sregex_iterator(line.begin(), line.end(), num_regex);
-  auto num_end = std::sregex_iterator();
-  std::vector<int64_t> time;
-  for (auto i = num_begin; i != num_end; ++i) {
-    time.push_back(stol((*i).str()));
-  }
-  std::getline(input, line);
-  num_begin = std::sregex_iterator(line.begin(), line.end(), num_regex);
-  num_end = std::sregex_iterator();
-  std::vector<int64_t> dist;
-  for (auto i = num_begin; i != num_end; ++i) {
-    dist.push_back(stol((*i).str()));
+
+  std::string time_line;
+  std::getline(input, time_line);
+  auto time_begin =
+      std::sregex_iterator(time_line.begin(), time_line.end(), num_regex);
+  auto time_end = std::sregex_iterator();
+
+  std::string dist_line;
+  std::getline(input, dist_line);
+  auto dist_begin =
+      std::sregex_iterator(dist_line.begin(), dist_line.end(), num_regex);
+  auto dist_end = std::sregex_iterator();
+
+  int64_t part1 = 1;
+  auto dist = dist_begin;
+  std::string time_p2 = "";
+  std::string dist_p2 = "";
+  for (auto time = time_begin; time != time_end; ++time, ++dist) {
+    time_p2 += (*time).str();
+    dist_p2 += (*dist).str();
+    auto t = stol((*time).str());
+    auto d = stol((*dist).str());
+    int64_t bot = ceil((t - sqrt(t * t - 4 * d)) / 2);
+    int64_t top = floor((t + sqrt(t * t - 4 * d)) / 2);
+    if (bot * (t - bot) <= d)
+      ++bot;
+    if (top * (t - top) <= d)
+      --top;
+    part1 *= (top - bot + 1);
   }
 
-  int64_t part1 = 0;
-  int64_t part2 = 0;
+  auto t = stol(time_p2);
+  auto d = stol(dist_p2);
+  int64_t bot = ceil((t - sqrt(t * t - 4 * d)) / 2);
+  int64_t top = floor((t + sqrt(t * t - 4 * d)) / 2);
+  if (bot * (t - bot) <= d)
+    ++bot;
+  if (top * (t - top) <= d)
+    --top;
+  auto part2 = (top - bot + 1);
 
   fmt::print("Day06: {} {}\n", part1, part2);
 }
